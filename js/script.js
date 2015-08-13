3e30b6519dba17224373e73f7c25e87bd411a3ae
reutrabin <reut.rabin@me.com>
2015-08-13 17:15:22 +0000

    hover and half working text

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
+  
 $(".info").addClass("hidden");
 $(".planet").click(function(){
          $(this).children(".info").toggleClass("hidden");
-      
-
+});
+  
+$(".name").addClass("hidden");
+$(".planet").hover(function(){
+         $(this).children(".name").toggleClass("hidden");
 });
 
  });
